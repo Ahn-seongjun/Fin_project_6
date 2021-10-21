21f45d191ac85dff94e4f27a3634b197ff0fd5cf
ankiyong <pop2413@gmail.com>
2021-10-21 17:22:10 +0900

    디장고 추가

--- django/Car/car_price/views.py
+++ django/Car/car_price/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 def index(request):
     return render(request,'index.html')
+
+def buy(request):
+    return render(request,'buy_page.html')
+
+def sell(request):
+    return render(request,'sell_page.html')
 # Create your views here.
