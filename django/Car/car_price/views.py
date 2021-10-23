98d0fe6643e3f97d19604e00f4a2d5f722d61698
ankiyong <pop2413@gmail.com>
2021-10-23 10:50:02 +0900

    디장고 수정

--- django/Car/car_price/views.py
+++ django/Car/car_price/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 def index(request):
-    return render(request,'index.html')
+    return render(request,'day_car.html')
 
 def buy(request):
     return render(request,'buy_page.html')
