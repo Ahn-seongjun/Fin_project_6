e7161d5b7bf1934b5457884020c02edc87fa2fd0
ankiyong <pop2413@gmail.com>
2021-10-25 20:44:50 +0900

    장고

--- django/Car/Car/settings.py
+++ django/Car/Car/settings.py
@@ -126,5 +126,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'car_price','templates')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'car_price','static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- django/Car/car_price/views.py
+++ django/Car/car_price/views.py
@@ -1,22 +1,25 @@
 from django.shortcuts import render
 from .models import Encar,Kcar
-def index(request):
 
-    return render(request,'day_car.html')
+
+def index(request):
+    return render(request,'day_car_main.html')
 
 def buy(request):
-    # s = EnCar.objects.filter(name__icontains = "2시리즈")
-     return render(request,'buy_page1.html')
+    return render(request,'car_buy.html')
 
 def search(request):
     bra = request.GET.get('brand','')
     na = request.GET.get('name','')
     site = request.GET.get('site', '')
+    year = request.GET.get('year','')
+    fuel = request.GET.get("fuel","")
+    loc = request.GET.get("loc","")
     if site == "Encar":
-        s = Encar.objects.filter(brand=bra,name__icontains=na)
+        s = Encar.objects.filter(brand=bra,name__icontains=na,year__gte=year,type=fuel,location=loc)
     else:
-        s = Kcar.objects.filter(brand=bra, name__icontains=na)
-    return render(request,'buy_page.html',{'brand':bra,'name':na,'s':s,'site':site})
+        s = Kcar.objects.filter(brand=bra, name__icontains=na,year__gte=year,type=fuel,location=loc)
+    return render(request,'car_search.html',{'brand':bra,'name':na,'s':s,'site':site,'year':year,'tyep':fuel,'location':loc})
 def sell(request):
     return render(request,'sell_page.html')
 # Create your views here.
