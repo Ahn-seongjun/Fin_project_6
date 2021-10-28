57d6fcd724ac33cda7fe9bb2b40f3ca260646d2b
ankiyong <pop2413@gmail.com>
2021-10-28 16:46:16 +0900

    django

--- Uesd_Car/Car/car_price/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from . import views
-urlpatterns = [
-    path('',views.main,name='main'),
-    path('sell',views.sell,name='sell'),
-    path('buy',views.buy,name='buy'),
-    path('search',views.search,name='search'),
-    ]
