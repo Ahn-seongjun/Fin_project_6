22ba72e17a24ec72d8cd29163adf9dcf3b580f01
ankiyong <pop2413@gmail.com>
2021-11-08 09:28:12 +0900

    1108

---
+++ Car_11_8/Daycar/my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default':{
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'data',
+        'USER': 'root',
+        'PASSWORD': '0000',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+
+    }
+}
+SECRET_KEY ='django-insecure-+zm(g+hh3!ypznrd=em@-tf)rkn*vfv-xugk)(w#m5levmux4v'
